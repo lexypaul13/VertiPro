62df201d560cadd7eb35ed871bb86ef51285e911
Mayowa Paul <lexypaul13@yahoo.com>
2024-09-29 01:02:27 -0400

    Initial Commit

---
+++ VertiPro/VertiProApp.swift
@@ -0,0 +1,17 @@
+//
+//  VertiProApp.swift
+//  VertiPro
+//
+//  Created by Alex Paul on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct VertiProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
